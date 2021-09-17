17a164a3ad4a0f141bdac5fecb678dba2344b22d
Andres Sanchez <42984427+LeonardoAndresSanchez@users.noreply.github.com>
2021-09-16 22:38:41 -0500

    remove coments

--- lib/src/utils/constantes.dart
+++ lib/src/utils/constantes.dart
@@ -1,10 +1,3 @@
 class Constantes {
-  // static String urlServer = 'http://192.168.0.25/services/';
-  //static String urlServer = 'http://192.168.0.63/services/';
-  // static String urlServer = 'https://luisfgrajales.com/services/';
   static String urlServer = 'http://34.86.199.231/navipuntos/services/';
-
-  // static String urlServer =
-  //     'http://gestiondelconocimiento2021.com/navipuntos/services/';
-  // static String urlServer = 'http://luisfgrajales.com/services/';
 }
